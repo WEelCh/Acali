fe3ab30992b810a0c19523450b3e4d4e3fca5a6a
WEelCh <github@weelch.de>
2026-06-16 19:26:59 +0200

    added weather and time locals

--- Locale.js
+++ Locale.js
@@ -7,6 +7,127 @@ APPLOC = "de";
 
 
 const Locale = {
+  dayPhase : [
+    /*morning*/   
+    { text : function(){return Locale.keyword.custom[APPLOC]},
+      de : "Spezial",
+      en : "Special",
+    },
+    /*day*/       
+    { text : function(){return Locale.keyword.custom[APPLOC]},
+      de : "Spezial",
+      en : "Special",
+    },
+    /*afternoon*/ 
+    { text : function(){return Locale.keyword.custom[APPLOC]},
+      de : "Spezial",
+      en : "Special",
+    },
+    /*night :: moon*/     [
+      /*losing*/  
+      { text : function(){return Locale.keyword.custom[APPLOC]},
+        de : "Spezial",
+        en : "Special",
+      },
+      /*new*/     
+      { text : function(){return Locale.keyword.custom[APPLOC]},
+        de : "Spezial",
+        en : "Special",
+      },
+      /*gaining*/ 
+      { text : function(){return Locale.keyword.custom[APPLOC]},
+        de : "Spezial",
+        en : "Special",
+      },
+      /*full*/    
+      { text : function(){return Locale.keyword.custom[APPLOC]},
+        de : "Spezial",
+        en : "Special",
+      },
+    ],
+  ],
+
+  weather : {
+    state : {
+      temp : [    
+        /*Arctic*/   
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Freezing*/ 
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Cold*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Medium*/   
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Warm*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+      ],
+      prec : [
+        /*Clear*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Cloudy*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Drizzle*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Rain*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Heavy*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+      ],
+      wind : [
+        /*Calm*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Breeze*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Gale*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+        /*Storm*/     
+        { text : function(){return Locale.keyword.custom[APPLOC]},
+          de : "Spezial",
+          en : "Special",
+        },
+      ],
+    },
+  },
+
   keyword : {
     custom : { text : function(){return Locale.keyword.custom[APPLOC]},
       de : "Spezial",
